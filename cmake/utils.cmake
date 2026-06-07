9964dd0e2d0a898d53d2d56a46106116c9851914
gamesbeash-art <gamesbeash@gmail.com>
2026-06-07 10:03:33 +0700

    Create utils.cmake

---
+++ cmake/utils.cmake
@@ -0,0 +1,10 @@
+# Utility macros for CMake build
+MACRO(RECURSE_FILES return_list filter)
+	FILE(GLOB_RECURSE new_list ${filter})
+	SET(file_list "")
+	FOREACH(file_path ${new_list})
+		SET(file_list ${file_list} ${file_path})
+	ENDFOREACH()
+	LIST(REMOVE_DUPLICATES file_list)
+	SET(${return_list} ${file_list})
+ENDMACRO()
