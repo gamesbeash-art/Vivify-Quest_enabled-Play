5e2220aa6e29366f9a80610ed86ffed4bbda199e
gamesbeash-art <gamesbeash@gmail.com>
2026-06-07 10:12:01 +0700

    Update qpm.cmake

--- cmake/qpm.cmake
+++ cmake/qpm.cmake
@@ -1,3 +1,4 @@
-# QPM Configuration
+# QPM Configuration - includes dependencies and external configuration
+
 include(${CMAKE_CURRENT_SOURCE_DIR}/qpm_defines.cmake)
 include(${CMAKE_CURRENT_SOURCE_DIR}/extern.cmake)
